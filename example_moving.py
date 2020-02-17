289aa6019792774371613b0d5c35a91223bc2560
Tori Colthurst <vrc2@illinois.edu>
2020-02-16 20:22:42 -0600

    arms

--- example_moving.py
+++ example_moving.py
@@ -15,6 +15,20 @@ clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
 if clientID == -1:
 	raise Exception('Failed connecting to remote API server')
 
+# Function that used to move joints
+def SetJointPosition():
+	print("attempting angles boyo")
+	vrep.simxSetJointPosition(clientID, armJoints[0], np.pi/2, vrep.simx_opmode_blocking)
+	time.sleep(0.5)
+	vrep.simxSetJointPosition(clientID, armJoints[1], -np.pi/4, vrep.simx_opmode_blocking)
+	time.sleep(0.5)
+	vrep.simxSetJointPosition(clientID, armJoints[2], np.pi/4, vrep.simx_opmode_blocking)
+	time.sleep(0.5)
+	vrep.simxSetJointPosition(clientID, armJoints[3], np.pi/4, vrep.simx_opmode_blocking)
+	time.sleep(0.5)
+	vrep.simxSetJointPosition(clientID, armJoints[4], np.pi/4, vrep.simx_opmode_blocking)
+	time.sleep(0.5)
+
 # ======================================== Setup "handle"  =========================================== #
 
 # Get a handle by name
@@ -52,16 +66,20 @@ def set_move(forwBackVel, leftRightVel, rotVel):
 # Start simulation
 vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
 
-forwBackVel = 10
+forwBackVel = 1
 leftRightVel = 0
-rotVel = 0
+rotVel = 2
 # Moving forwards at 10
 set_move(forwBackVel, leftRightVel, rotVel)
+# Goal_joint_angles = np.array([[0,0,-0.5*np.pi,0.5*np.pi,-0.5*np.pi,-0.5*np.pi], \
+# 							[0.5*np.pi,0,-0.5*np.pi,0.5*np.pi,0.5*np.pi,-0.5*np.pi],\
+# 							[-0.5*np.pi,-0.5*np.pi,-0.5*np.pi,0,-0.5*np.pi,-0.5*np.pi]])
+SetJointPosition()
 
 # ******************************** Your robot control code goes here  ******************************** #
 
 # Wait two seconds
-for i in range(1000):
+for i in range(100):
     result = vrep.simxReadProximitySensor(clientID, prox_sensor, vrep.simx_opmode_blocking)
     print(result)
     time.sleep(0.01)
