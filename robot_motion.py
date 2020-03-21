5ce899bc20288eea1707aa4d774241c0a7ac3fd0
Tori Colthurst <vrc2@illinois.edu>
2020-03-21 16:08:44 -0500

    temp start

---
+++ robot_motion.py
@@ -0,0 +1,54 @@
+import vrep
+import time
+import numpy as np
+from math import cos, sin
+from scipy.linalg import expm,logm
+
+# Close all open connections (Clear bad cache)
+vrep.simxFinish(-1)
+# Connect to V-REP (raise exception on failure)
+clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+if clientID == -1:
+	raise Exception('Failed connecting to remote API server')
+
+# ======================================== Setup "handle"  ============================================= #
+
+# Get a handle by name
+def get_handle_blocking(name):
+    result, obj = vrep.simxGetObjectHandle(clientID, name, vrep.simx_opmode_blocking)
+    if result != vrep.simx_return_ok:
+        raise Exception('could not get object: {}'.format(name))
+    return obj
+
+wheelJoints = [-1, -1, -1, -1]
+wheelJoints[0]  = get_handle_blocking('rollingJoint_fl')
+wheelJoints[1]  = get_handle_blocking('rollingJoint_rl')
+wheelJoints[2]  = get_handle_blocking('rollingJoint_rr')
+wheelJoints[3]  = get_handle_blocking('rollingJoint_fr')
+youBot          = get_handle_blocking('youBot')
+youBotRef       = get_handle_blocking('youBot_ref')
+tip             = get_handle_blocking('youBot_positionTip')
+target          = get_handle_blocking('youBot_positionTarget')
+prox_sensor     = get_handle_blocking('Proximity_sensor')
+lidar_motor     = get_handle_blocking('Tower_Turning_Joint')
+
+armJoints = [-1] * 5
+for i in range(5):
+    armJoints[i] = get_handle_blocking('youBotArmJoint{}'.format(i))
+
+# ======================================================================================================= #
+# ======================================= Start Simulation ============================================== #
+# ======================================================================================================= #
+
+vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
+
+# ======================================================================================================= #
+# ======================================== End Simulation =============================================== #
+# ======================================================================================================= #
+
+vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+# Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+vrep.simxGetPingTime(clientID)
+# Close the connection to V-REP
+vrep.simxFinish(clientID)
+print("==================== ** Simulation Ended ** ====================")
