8485a711a0ddc674f820dc8dd821605db37a30cc
Tori Colthurst <vrc2@illinois.edu>
2020-03-22 04:48:52 -0500

    code for arms doesn't work yet but its 5 am

--- main_sim.py
+++ main_sim.py
@@ -5,6 +5,7 @@ import math
 from scipy.linalg import expm, logm
 import matplotlib.pyplot as plt
 from scripts.robot_motion import *
+from scripts.arm_motion import *
 from scripts.robot_lidar import *
 
 # Close all open connections (Clear bad cache)
@@ -34,6 +35,7 @@ tip             = get_handle_blocking('youBot_positionTip')
 target          = get_handle_blocking('youBot_positionTarget')
 prox_sensor     = get_handle_blocking('Proximity_sensor')
 lidar_motor     = get_handle_blocking('Tower_Turning_Joint')
+gripper 		= get_handle_blocking('youBotGripperJoint1')
 
 armJoints = [-1] * 5
 for i in range(5):
@@ -68,35 +70,48 @@ robot_motion.set_move_global_position(endpos, 0.01)
 robot_motion.get_global_position()
 robot_motion.get_global_orientation()
 
+arm_motion = arm_motion(clientID, youBotRef, armJoints, youBot, gripper)
+thetas = [math.pi/2, -math.pi/4, math.pi/4, math.pi/4, math.pi/4]
+prediction = arm_motion.forw_kin(thetas)
+print("PREDICTION")
+print(prediction)
+print()
+arm_motion.SetJointPosition(thetas)
+print("ACTUAL")
+read_pos = arm_motion.get_any_ref_position(gripper, youBot)
+print()
+
+time.sleep(5)
+
 # Simulation dt is 50ms (0.05s)
-dt = 0.05
-
-while True:
-    # Trigger a "tick"
-    vrep.simxSynchronousTrigger(clientID)
-    vrep.simxGetPingTime(clientID)
-    if not robot_motion.motion_update():
-        break
-
-print("Scanning...")
-points = []
-for i in range(100):
-    # Trigger a "tick"
-    vrep.simxSynchronousTrigger(clientID)
-    vrep.simxGetPingTime(clientID)
-    
-    pt = robot_lidar.read_lidar_point()
-    if pt:
-        points.append(pt)
-        
-robot_motion.set_move(0,0,0)
-vrep.simxSynchronousTrigger(clientID)
-
-
-points = np.array(points).T
-# print(points)
-plt.plot(points[0], points[1], 'o')
-plt.show()
+# dt = 0.05
+#
+# while True:
+#     # Trigger a "tick"
+#     vrep.simxSynchronousTrigger(clientID)
+#     vrep.simxGetPingTime(clientID)
+#     if not robot_motion.motion_update():
+#         break
+#
+# print("Scanning...")
+# points = []
+# for i in range(100):
+#     # Trigger a "tick"
+#     vrep.simxSynchronousTrigger(clientID)
+#     vrep.simxGetPingTime(clientID)
+#
+#     pt = robot_lidar.read_lidar_point()
+#     if pt:
+#         points.append(pt)
+#
+# robot_motion.set_move(0,0,0)
+# vrep.simxSynchronousTrigger(clientID)
+#
+#
+# points = np.array(points).T
+# # print(points)
+# plt.plot(points[0], points[1], 'o')
+# plt.show()
 
 # ======================================================================================================= #
 # ======================================== End Simulation =============================================== #
--- scripts/arm_motion.py
+++ scripts/arm_motion.py
@@ -8,39 +8,42 @@ absolute_position = -1
 
 # ======================================= Helper Functions ============================================== #
 class arm_motion:
-	""" Class for the 2D motion of the entire robot frame. """
+    """ Class for the 2D motion of the entire robot frame. """
 
-	def __init__(self, clientID, youBotRef, arms, youBot):
-		""" Initialize a motion object. """
-		self.clientID = clientID
-		self.youBotRef = youBotRef
+    def __init__(self, clientID, youBotRef, arms, youBot, gripper):
+        """ Initialize a motion object. """
+        self.clientID = clientID
+        self.youBotRef = youBotRef
         self.youBot = youBot
-		self.arms = arms
+        self.arms = arms
+        self.gripper = gripper
 
         # forward kinematics for arm joints
         # zero position, relative to youBot frame
-        self.w = [[1,0,0],[0,1,0],[0,1,0],[0,1,0],[1,0,0]]
+        self.w = np.array([[1,0,0],[0,1,0],[0,1,0],[0,1,0],[1,0,0]])
         self.r = []
         self.v = []
         for arm, w in zip(self.arms,self.w):
-            r = get_any_ref_position(arm, self.youBot)
+            r = self.get_any_ref_position(arm, self.youBot)
             self.r.append(r)
             self.v.append(np.cross(-w,r))
+        pos = self.get_any_ref_position(gripper, self.youBot)
+        self.M = np.array([[-1,0,0,pos[0]],[0,0,1,pos[1]],[0,1,0,pos[2]],[0,0,0,1]])
 
-	def get_any_ref_position(self, handle, reference):
-		error,position = vrep.simxGetObjectPosition(self.clientID, handle, reference, vrep.simx_opmode_blocking)
-		if error:
-			print("get_global_position ERROR")
-		print("POSITION: x="+str(position[0])+" y="+str(position[1])+" z="+str(position[2])+"\n")
-		return position
+    def get_any_ref_position(self, handle, reference):
+        error,position = vrep.simxGetObjectPosition(self.clientID, handle, reference, vrep.simx_opmode_blocking)
+        if error:
+            print("get_global_position ERROR")
+        print("POSITION: x="+str(position[0])+" y="+str(position[1])+" z="+str(position[2])+"\n")
+        return position
 
-    def forw_kin(self, M, thetas):
+    def forw_kin(self, thetas):
         e_s_theta = []
         for w, v, theta in zip(self.w,self.v,thetas):
             w_skew = np.array([[0,-w[2],w[1]], [w[2],0,-w[0]], [-w[1],w[0],0]])
             temp = np.eye(3) + np.sin(theta)*w_skew + (1-np.cos(theta))*(w_skew@w_skew)
-            temp2 = (np.eye(3)*theta + (1-np.cos(the))*w_skew + \
-                    (the-np.sin(theta))*(w_skew@w_skew))@v
+            temp2 = (np.eye(3)*theta + (1-np.cos(theta))*w_skew + \
+                    (theta-np.sin(theta))*(w_skew@w_skew))@v
 
             temp3 = np.zeros((4,4))
             temp3[0:3, 0:3] = temp
@@ -51,4 +54,9 @@ class arm_motion:
         answer = e_s_theta[0]
         for i in range(1, len(e_s_theta)):
             answer = answer@e_s_theta[i]
-        answer = answer@M
+        answer = answer@self.M
+        return answer
+
+    def SetJointPosition(self, thetas):
+        for arm, theta in zip(self.arms, thetas):
+            vrep.simxSetJointPosition(self.clientID, arm, theta, vrep.simx_opmode_blocking)
