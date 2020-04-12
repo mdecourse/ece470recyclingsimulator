730df10c0a050d87bbc4ecadbed18341820d6eeb
Tori Colthurst <vrc2@illinois.edu>
2020-04-12 15:28:13 -0500

    manual

--- main_sim.py
+++ main_sim.py
@@ -76,6 +76,8 @@ vision_sensor = vision_sensor(clientID, vision_sens)
 robot_lidar = robot_lidar(clientID, prox_sensor, lidar_motor)
 robot_lidar.set_lidar_velocity(lidar_v)
 
+manual_mode = 1
+
 vfb = 0
 vlr = 0
 vt = 0
@@ -91,7 +93,7 @@ def update_pf():
         readings.append(lidar_result)
     else:
         readings.append(5)
-    
+
     pf.update(robot_motion.velocities[0], robot_motion.velocities[1], robot_motion.velocities[2], robot_lidar.get_lidar_angle() + np.pi / 2, dt)
     i += 1
     if i == 30:
@@ -108,82 +110,84 @@ def update_pf():
         pos = robot_motion.get_global_position()
         print("Actual:[{} {}]".format(pos[0] + 2, pos[1] + 2))
 
-print("Localizing and preparing dijkstras tables...")
-keep_going = True
-def dijkstras_run_thread():
-    global keep_going
-    setup_dijkstras(show_dijkstra)
-    keep_going = False
-    
-th.Thread(target=dijkstras_run_thread, args=(), name='dijkstras_run_thread', daemon=True).start()
-robot_motion.set_move(0.2, 0, 0)
-last_n_pf_updates = n_pf_updates
-while keep_going or n_pf_updates < 12:
-    #break
-    vrep.simxSynchronousTrigger(clientID)
-    vrep.simxGetPingTime(clientID)
-    update_pf()
-    if n_pf_updates == last_n_pf_updates+4:
-        robot_motion.set_move(-robot_motion.velocities[0], 0, 0)
-        last_n_pf_updates = n_pf_updates
-    robot_motion.motion_update()
-    arm_motion.motion_update()
-    
-pf.resample_particles = 40
+if not manual_mode:
+    print("Localizing and preparing dijkstras tables...")
+    keep_going = True
+    def dijkstras_run_thread():
+        global keep_going
+        setup_dijkstras(show_dijkstra)
+        keep_going = False
 
-keep_going = True
-target_point = (1, 2)
-print("Pathing mode: Going to {}".format(target_point))
-robot_motion.set_move_global_position2(target_point, get_local_heading, lambda: pf.get_predicted_pose(), 0.25)
-while keep_going:
-    #break
-    vrep.simxSynchronousTrigger(clientID)
-    vrep.simxGetPingTime(clientID)
-    update_pf()
-    keep_going = robot_motion.motion_update()
-    arm_motion.motion_update()
-    
+    th.Thread(target=dijkstras_run_thread, args=(), name='dijkstras_run_thread', daemon=True).start()
+    robot_motion.set_move(0.2, 0, 0)
+    last_n_pf_updates = n_pf_updates
+    while keep_going or n_pf_updates < 12:
+        #break
+        vrep.simxSynchronousTrigger(clientID)
+        vrep.simxGetPingTime(clientID)
+        update_pf()
+        if n_pf_updates == last_n_pf_updates+4:
+            robot_motion.set_move(-robot_motion.velocities[0], 0, 0)
+            last_n_pf_updates = n_pf_updates
+        robot_motion.motion_update()
+        arm_motion.motion_update()
 
-print("Manual mode")
+    pf.resample_particles = 40
 
-# Hack to do manual robot control
-keep_going = True
+    keep_going = True
+    target_point = (1, 2)
+    print("Pathing mode: Going to {}".format(target_point))
+    robot_motion.set_move_global_position2(target_point, get_local_heading, lambda: pf.get_predicted_pose(), 0.25)
+    while keep_going:
+        #break
+        vrep.simxSynchronousTrigger(clientID)
+        vrep.simxGetPingTime(clientID)
+        update_pf()
+        keep_going = robot_motion.motion_update()
+        arm_motion.motion_update()
 
-def key_capture_thread():
-    global keep_going
-    global vfb
-    global vlr
-    global vt
-    while True:
-        in_str = input().strip()
-        for c in in_str:
-            if c == "q":
-                keep_going = False
-                break
-            elif c == "w":
-                vfb += 0.1
-            elif c == "s":
-                vfb -= 0.1
-            elif c == "d":
-                vlr += 0.1
-            elif c == "a":
-                vlr -= 0.1
-            elif c == "x":
-                vt -= 0.2
-            elif c == "z":
-                vt += 0.2
-                
-th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
-while keep_going:
-    robot_motion.set_move(vfb, vlr, vt)
-    vision_sensor.read_sensor()
-    # Trigger a "tick"
-    vrep.simxSynchronousTrigger(clientID)
-    vrep.simxGetPingTime(clientID)
-    update_pf()
+elif manual_mode:
 
-    robot_motion.motion_update()
-    arm_motion.motion_update()
+    print("Manual mode")
+
+    # Hack to do manual robot control
+    keep_going = True
+
+    def key_capture_thread():
+        global keep_going
+        global vfb
+        global vlr
+        global vt
+        while True:
+            in_str = input().strip()
+            for c in in_str:
+                if c == "q":
+                    keep_going = False
+                    break
+                elif c == "w":
+                    vfb += 0.1
+                elif c == "s":
+                    vfb -= 0.1
+                elif c == "d":
+                    vlr += 0.1
+                elif c == "a":
+                    vlr -= 0.1
+                elif c == "x":
+                    vt -= 0.2
+                elif c == "z":
+                    vt += 0.2
+
+    th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
+    while keep_going:
+        robot_motion.set_move(vfb, vlr, vt)
+        vision_sensor.read_sensor()
+        # Trigger a "tick"
+        vrep.simxSynchronousTrigger(clientID)
+        vrep.simxGetPingTime(clientID)
+        update_pf()
+
+        robot_motion.motion_update()
+        arm_motion.motion_update()
 
 # robot_motion.set_move(0,0,0)
 # pos = robot_motion.get_global_position()
--- scripts/vision.py
+++ scripts/vision.py
@@ -23,27 +23,18 @@ class vision_sensor:
     def read_sensor(self):
         # returnCode, detectionState, auxPackets = \
         #     vrep.simxReadVisionSensor(self.clientID, self.sensor, vrep.simx_opmode_buffer)
-        # if returnCode == 0 and len(auxPackets) > 0:
-        #     # print(auxPackets)
-        #     avg_red = auxPackets[0][11]
-        #     # print("avg red color: " + str(avg_red))
-        #     # avg_green = auxPackets[0][12]
-        #     # avg_blue = auxPackets[0][13]
-        #     # avg_red = avg_red / (avg_red + avg_green + avg_blue)
-        #     # max_red = auxPackets[0][6]
-        #     if avg_red > 0.39:
-        #         print("avg is BIG RED")
-        #     # if max_red > self.max_red:
-        #     #     print("max red color: " + str(max_red))
         returnCode, resolution, image = \
             vrep.simxGetVisionSensorImage(self.clientID, self.sensor, 0, vrep.simx_opmode_buffer)
         returnCode, resolution, depth = \
             vrep.simxGetVisionSensorDepthBuffer(self.clientID, self.sensor, vrep.simx_opmode_buffer)
+        num_pixels = 0
         if len(resolution) > 0:
             for x in range(0, resolution[0]):
                 for y in range(0, resolution[1]):
                     red = image[(x*resolution[0]+y)*3]& 0b000011111111
                     if red > 250:
+                        num_pixels += 1
                         d = depth[x*resolution[0]+y]
-                        print("Cylinder is: "+str(d)+" meters away.")
+                        # print("Cylinder is: "+str(d)+" meters away.")
                         # print("yay red")
+            print("Number of red pixels: " + str(num_pixels))
