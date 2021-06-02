1f8d309260addf0fe55f1cf720eea41e9b349f58
mdecourse <mdeourse@gmail.com>
2021-06-02 10:47:27 +0800

    convert project to pj2022

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = True 
-    site_title = "Pj5073"
+    site_title = "pj2022"
     # for IPv4
     ip = "127.0.0.1"
     # for IPv6
