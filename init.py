6d5d19d3a68b298e10e69ff926f74fd8698902cc
mdecourse <mdecourse@gmail.com>
2021-09-06 22:37:41 +0800

    update cmsimde

--- init.py
+++ init.py
@@ -10,12 +10,10 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "pj2022"
-    # for IPv4
+    site_title = "PJ2022"
     ip = "127.0.0.1"
-    # for IPv6
-    #ip = "::"
-    port = 9445
+    dynamic_port = 9449
+    static_port = 8449
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
